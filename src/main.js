c2c16ca4620268b8a10eee1b0526b2e379ee3dea
Ayoola Ololade <ayoola.ololade2014@gmail.com>
2023-12-07 20:24:36 +0100

    to do

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 
-createApp(App).mount('#app')
+// Vue.config.productionTip = false;
+
+// new Vue({
+//   render: (h) => h(App),
+// }).$mount("#app");
+createApp(App).mount("#app");
